7087ecf0214b045ecaf24733b99f753b3a164628
NavedAhemad-Java <navedahemad20@gmail.com>
2023-04-05 15:58:39 +0530

    Completed productRespository

---
+++ src/main/java/com/bikkadIt/electronic/store/respository/ProductRepository.java
@@ -0,0 +1,22 @@
+package com.bikkadIt.electronic.store.respository;
+
+import com.bikkadIt.electronic.store.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    //search
+    Page<Product>findByTitleContaining(String subTitle,Pageable pageable);
+    Page<Product> findByLiveTrue(Pageable pageable);
+
+    //other methods
+    //custom finder methods
+    //uery methods
+
+
+}
