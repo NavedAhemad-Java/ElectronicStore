5586fa8e395038b1bd478a827a910c71711f3ce1
NavedAhemad-Java <navedahemad20@gmail.com>
2023-04-05 15:53:47 +0530

    Completed CategoryRepository

--- src/main/java/com/bikkadIt/electronic/store/respository/CategoryRepository.java
+++ src/main/java/com/bikkadIt/electronic/store/respository/CategoryRepository.java
@@ -1,12 +1,11 @@
 package com.bikkadIt.electronic.store.respository;
 
 import com.bikkadIt.electronic.store.entities.Category;
-import com.bikkadIt.electronic.store.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CategoryRespository extends JpaRepository<Category,Long> {
+public interface CategoryRepository extends JpaRepository<Category,Long> {
 
     List<Category> findByTitleContaining(String keyword);
 }
