fdc0375ca88217ab81efa2b72f946845ce4d7bfb
NavedAhemad-Java <navedahemad20@gmail.com>
2023-04-24 18:12:31 +0530

    Completed

--- src/main/java/com/bikkadIt/electronic/store/ElectronicStoreApplication.java
+++ src/main/java/com/bikkadIt/electronic/store/ElectronicStoreApplication.java
@@ -1,14 +1,24 @@
 package com.bikkadIt.electronic.store;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-public class ElectronicStoreApplication {
+@EnableWebMvc
+
+public class ElectronicStoreApplication implements CommandLineRunner  {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(ElectronicStoreApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
